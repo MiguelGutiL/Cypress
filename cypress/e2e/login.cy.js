8f5f37f923eed0f236be901b6f5850616359267b
Miguel Gutierrez <magutierrezledesma@gmail.com>
2026-08-10 21:28:46 -0600

    Login validations

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,39 @@
-
+import LoginPage from '../pages/loginPage';
+//import Products from '../pages/Products';
 
 describe('Login tests', () =>{
 
+    beforeEach(() =>{
+        cy.visit('/');    
+    });
+
+    /*after(()=>{
+        LoginPage.logout();
+    });*/
+
     it('Success login using the correct credentials', () =>{
-        cy.visit('/');
-        cy.get('body').should('be.visible');
-    })
+        cy.fixture('users').then($data => {
+            LoginPage.login($data.validUser.username, $data.validUser.password);
+            cy.url().should('include', '/inventory');
+        });
+        LoginPage.logout();
+    });
+
+    it('Validate that locked User is not login and showing the respective message', ()=>{
+        cy.fixture('users').then($data => {
+            LoginPage.login($data.lockedUser.username, $data.lockedUser.password);
+            LoginPage.loginPage.lockedUserMessage().should('exist').invoke('text').should('include','Epic sadface: Sorry, this user has been locked out.');
+        });
+        LoginPage.loginPage.lockedCloseUserMsg().click();
+    });
+
+    it('Validate the user login and the images issue', () =>{
+        cy.fixture('users').then($data => {
+            LoginPage.login($data.problemUser.username, $data.problemUser.password);
+            cy.url().should('include', '/inventory');
+        });
+
+        //Products.products.productImages().invoke('attr','src').should('include','/assets/sl-404-Cq1a9k9X.jpg');
+        LoginPage.logout();
+    });
 });
